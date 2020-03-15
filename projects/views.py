a74512009b98d00de04f8a00797d45d4ed347ea1
amirranjbar <amir_abbas_r@yahoo.com>
2020-03-15 11:51:47 +0330

    complete project part

--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,12 @@ from projects.models import Project
 
 
 def project_index(request):
-    projects = Project.object.all()
-    context = {
-        'project': projects
-    }
-    return render(request.'project_index.html',
-                  context)
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "project_index.html", context)
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {"project": project}
+    return render(request, "project_detail.html", context)

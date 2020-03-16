1546ee4a141a5a97238a3c65be4f0a663c862804
amirranjbar <amir_abbas_r@yahoo.com>
2020-03-16 10:41:41 +0330

    project image

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'technology']
+    list_filter = ['technology']
+    #list_editable = ['description', 'technology']
+    #prepopulated_fields = {'technology': ('title',)}
+
+
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from personal_portfolio import settings
+import os
 
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    # image = models.FilePathField(path=os.path.join(settings.BASE_DIR, 'projects/static/img'))
+    # image = models.FilePathField(path="/img")
+    # image = models.FileField(upload_to="projects/static/img/")
+    image = models.ImageField(upload_to='projects/static/img')

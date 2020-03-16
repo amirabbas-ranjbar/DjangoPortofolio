99fbe91a0212b44f64568f1643cad7329edf81b8
amirranjbar <amir_abbas_r@yahoo.com>
2020-03-16 11:01:44 +0330

    create  blog project

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'projects',
-    
-    
+    'blog',
+
+
 ]
 
 MIDDLEWARE = [
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    # path("blog/", include("blog.urls")),
     path("", include("projects.urls")),
 ]
 if settings.DEBUG:

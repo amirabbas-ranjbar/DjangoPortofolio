4e85f3518df4455aa4a1a28b1582475ce2d43f68
amirranjbar <amir_abbas_r@yahoo.com>
2020-03-17 23:35:02 +0330

    admin blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Comment, Post
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'created_on']
+    list_filter = ['author','created_on']
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_on', 'last_modified']
+    list_filter = ['created_on','last_modified','categories']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Post, PostAdmin)
